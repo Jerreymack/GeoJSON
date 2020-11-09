debc301c535f46668d14b0672f18c58caf5c8f06
Jerreymack <72449159+Jerreymack@users.noreply.github.com>
2020-11-09 11:23:40 -0500

    Create bus_GeoJSONP.js

---
+++ bus_GeoJSONP.js
@@ -0,0 +1,36 @@
+bus_callback({ "type": "FeatureCollection",
+
+  "features": [
+
+
+    { "type": "Feature",
+
+      "geometry": {"type": "Point", "coordinates": [48.832268, -67.567709]},
+
+      "properties": {"prop0": "value0"}
+
+      },
+    { "type": "Feature",
+      "geometry": {"type": "Point", "coordinates": [48.847146, -67.519258]},
+      "properties": {"prop0": "value0"}
+      },
+    { "type": "Feature",
+      "geometry": {"type": "Point", "coordinates": [48.847880, -67.542261]},
+      "properties": {"prop0": "value0"}
+      },
+    { "type": "Feature",
+      "geometry": {"type": "Point", "coordinates": [48.834662, -67.535566]},
+      "properties": {"prop0": "value0"}
+      },
+    { "type": "Feature",
+      "geometry": {"type": "Point", "coordinates": [48.849631, -67.549428]},
+      "properties": {"prop0": "value0"}
+      },
+    { "type": "Feature",
+      "geometry": {"type": "Point", "coordinates": [48.844520, -67.532991]},
+      "properties": {"prop0": "value0"}
+      }
+
+ ]
+
+})
