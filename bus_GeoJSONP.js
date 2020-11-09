29581dee9415b6d099259f634d55f18d18549b00
Jerreymack <72449159+Jerreymack@users.noreply.github.com>
2020-11-09 12:25:51 -0500

    Update bus_GeoJSONP.js

--- bus_GeoJSONP.js
+++ bus_GeoJSONP.js
@@ -1,36 +1,83 @@
-bus_callback({ "type": "FeatureCollection",
-
+{
+  "type": "FeatureCollection",
   "features": [
-
-
-    { "type": "Feature",
-
-      "geometry": {"type": "Point", "coordinates": [48.832268, -67.567709]},
-
-      "properties": {"prop0": "value0"}
-
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          48.832268,
+          -67.567709
+        ]
       },
-    { "type": "Feature",
-      "geometry": {"type": "Point", "coordinates": [48.847146, -67.519258]},
-      "properties": {"prop0": "value0"}
-      },
-    { "type": "Feature",
-      "geometry": {"type": "Point", "coordinates": [48.847880, -67.542261]},
-      "properties": {"prop0": "value0"}
-      },
-    { "type": "Feature",
-      "geometry": {"type": "Point", "coordinates": [48.834662, -67.535566]},
-      "properties": {"prop0": "value0"}
-      },
-    { "type": "Feature",
-      "geometry": {"type": "Point", "coordinates": [48.849631, -67.549428]},
-      "properties": {"prop0": "value0"}
-      },
-    { "type": "Feature",
-      "geometry": {"type": "Point", "coordinates": [48.844520, -67.532991]},
-      "properties": {"prop0": "value0"}
+      "properties": {
+        "prop0": "value0"
       }
-
- ]
-
-})
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          48.847146,
+          -67.519258
+        ]
+      },
+      "properties": {
+        "prop0": "value0"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          48.84788,
+          -67.542261
+        ]
+      },
+      "properties": {
+        "prop0": "value0"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          48.834662,
+          -67.535566
+        ]
+      },
+      "properties": {
+        "prop0": "value0"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          48.849631,
+          -67.549428
+        ]
+      },
+      "properties": {
+        "prop0": "value0"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          48.84452,
+          -67.532991
+        ]
+      },
+      "properties": {
+        "prop0": "value0"
+      }
+    }
+  ]
+}
