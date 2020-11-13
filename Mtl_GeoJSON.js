e9f105acd6770fa71fa671addc63f583217dcb3f
Jerreymack <72449159+Jerreymack@users.noreply.github.com>
2020-11-12 22:37:31 -0500

    Create Mtl_GeoJSON.js

---
+++ Mtl_GeoJSON.js
@@ -0,0 +1,117 @@
+bus_callback({
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "plate": "T145879",
+        "coordinates": [
+          45.497053, 
+          -73.555630
+        ]
+      },
+      "properties": {
+        "prop0": "value0"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "plate": "T335794",
+        "coordinates": [
+          45.509451, 
+          -73.560941
+        ]
+      },
+      "properties": {
+        "prop0": "value0"
+      }
+    },
+    {
+      "type": "Feature",
+      "plate": "T977482",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          45.493910, 
+          -73.574792
+        ]
+      },
+      "properties": {
+        "prop0": "value0"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "plate": "T732027",
+        "type": "Point",
+        "coordinates": [
+          45.488419, 
+          -73.588782
+        ]
+      },
+      "properties": {
+        "prop0": "value0"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "plate": "T322987",
+        "coordinates": [
+          45.509451, 
+          -73.560941
+        ]
+      },
+      "properties": {
+        "prop0": "value0"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "plate": "554896",
+        "coordinates": [
+          45.479934, 
+          -73.556446
+        ]
+      },
+      "properties": {
+        "prop0": "value0"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "plate": "T145879",
+        "coordinates": [
+          45.465986, 
+          -73.543507
+        ]
+      },
+      "properties": {
+        "prop0": "value0"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "plate": "T145879",
+        "coordinates": [
+          45.496512, 
+          -73.544708
+        ]
+      },
+      "properties": {
+        "prop0": "value0"
+      }
+    }
+  ]
+})
